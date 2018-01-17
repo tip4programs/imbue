134c4c62fb70359854952fa360e58142ce96bb74
chipprogrammers <arunksoman5678@gmail.com>
2018-01-17 23:00:26 +0530

    Update super-search.js

--- asset/js/super-search.js
+++ asset/js/super-search.js
@@ -1,89 +1,144 @@
-(function($) {
-    $.fn.simpleJekyllSearch = function(options) {
-        var settings = $.extend({
-            jsonFile        : '/search.json',
-            jsonFormat      : 'title,tags,categories,url,date',
-            template : '<li><article><a href="{url}">{title} <span class="entry-date"><time datetime="{date}">{date}</time></span></a></article></li>',
-            searchResults   : '.search-results',
-            searchResultsTitle   : '<h4>Search results:</h4>',
-            limit           : '10',
-            noResults       : '<p>Oh snap!<br/><small>We found nothing :(</small></p>'
-        }, options);
+/* super-search
+Author: Kushagra Gour (http://kushagragour.in)
+MIT Licensed
+*/
+(function () {
+	var searchFile = '/feed.xml',
+		searchEl,
+		searchInputEl,
+		searchResultsEl,
+		currentInputValue = '',
+		lastSearchResultHash,
+		posts = [];
 
-        var properties = settings.jsonFormat.split(',');
+	// Changes XML to JSON
+	// Modified version from here: http://davidwalsh.name/convert-xml-json
+	function xmlToJson(xml) {
+		// Create the return object
+		var obj = {};
+		if (xml.nodeType == 3) { // text
+			obj = xml.nodeValue;
+		}
 
-        var jsonData = [],
-            origThis = this,
-            searchResults = $(settings.searchResults);
+		// do children
+		// If all text nodes inside, get concatenated text from them.
+		var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
+		if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
+			obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
+		}
+		else if (xml.hasChildNodes()) {
+			for(var i = 0; i < xml.childNodes.length; i++) {
+				var item = xml.childNodes.item(i);
+				var nodeName = item.nodeName;
+				if (typeof(obj[nodeName]) == "undefined") {
+					obj[nodeName] = xmlToJson(item);
+				} else {
+					if (typeof(obj[nodeName].push) == "undefined") {
+						var old = obj[nodeName];
+						obj[nodeName] = [];
+						obj[nodeName].push(old);
+					}
+					obj[nodeName].push(xmlToJson(item));
+				}
+			}
+		}
+		return obj;
+	}
 
-        if(settings.jsonFile.length && searchResults.length){
-            $.ajax({
-                type: "GET",
-                url: settings.jsonFile,
-                dataType: 'json',
-                success: function(data, textStatus, jqXHR) {
-                    jsonData = data;
-                    registerEvent();
-                },
-                error: function(x,y,z) {
-                    console.log("***ERROR in simpleJekyllSearch.js***");
-                    console.log(x);
-                    console.log(y);
-                    console.log(z);
-                    // x.responseText should have what's wrong
-                }
-            });
-        }
+	function getPostsFromXml(xml) {
+		var json = xmlToJson(xml);
+		// Atom 1.0 format
+		if (json.entry && json.entry instanceof Array) {
+			return json.entry;
+		}
+		// Atom 2.0 format
+		else {
+			return json.channel.item;
+		}
+	}
 
+	window.toggleSearch = function toggleSearch() {
+		searchEl.classList.toggle('is-active');
+		if (searchEl.classList.contains('is-active')) {
+			// while opening
+			searchInputEl.value = '';
+		} else {
+			// while closing
+			searchResultsEl.classList.add('is-hidden');
+		}
+		setTimeout(function () {
+			searchInputEl.focus();
+		}, 210);
+	}
 
-        function registerEvent(){
-            origThis.keyup(function(e){
-                if($(this).val().length){
-                    writeMatches( performSearch($(this).val()));
-                }else{
-                    clearSearchResults();
-                }
-            });
-        }
+	function handleInput() {
+		var currentResultHash, d;
 
-        function performSearch(str){
-            var matches = [];
+		currentInputValue = (searchInputEl.value + '').toLowerCase();
+		if (!currentInputValue || currentInputValue.length < 3) {
+			lastSearchResultHash = '';
+			searchResultsEl.classList.add('is-hidden');
+			return;
+		}
+		searchResultsEl.style.offsetWidth;
 
-            $.each(jsonData,function(i,entry){
-                for(var i=0;i<properties.length;i++)
-                    if(entry[properties[i]] !== undefined && entry[properties[i]].toLowerCase().indexOf(str.toLowerCase()) !== -1){
-                        matches.push(entry);
-                        i=properties.length;
-                    }
-            });
-            return matches;
+		var matchingPosts = posts.filter(function (post) {
+			// Search `description` and `content` both to support 1.0 and 2.0 formats.
+			if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || ((post.description || post.content) + '').toLowerCase().indexOf(currentInputValue) !== -1) {
+				return true;
+			}
+		});
+		if (!matchingPosts.length) {
+			searchResultsEl.classList.add('is-hidden');
+		}
+		currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
+		if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
+			searchResultsEl.classList.remove('is-hidden');
+			searchResultsEl.innerHTML = matchingPosts.map(function (post) {
+				d = new Date(post.pubDate);
+				return '<li><a href="' + post.link + '">' + post.title + '<span class="super-search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
+			}).join('');
+		}
+		lastSearchResultHash = currentResultHash;
+	}
 
-        }
+	function init(options) {
+		searchFile = options.searchFile || searchFile;
+		searchEl = document.querySelector(options.searchSelector || '#js-super-search');
+		searchInputEl = document.querySelector(options.inputSelector || '#js-super-search__input');
+		searchResultsEl = document.querySelector(options.resultsSelector || '#js-super-search__results');
 
-        function writeMatches(m) {
-            clearSearchResults();
-            searchResults.append( $(settings.searchResultsTitle) );
+		var xmlhttp=new XMLHttpRequest();
+		xmlhttp.open('GET', searchFile);
+		xmlhttp.onreadystatechange = function () {
+			if (xmlhttp.readyState != 4) return;
+			if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
+			var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
+			node = node.children[0];
+			posts = getPostsFromXml(node);
+		}
+		xmlhttp.send();
 
-            if(m.length){
-                $.each(m,function(i,entry){
-                    if(i<settings.limit){
-                        var output=settings.template;
-                        for(var i=0;i<properties.length;i++){
-                            var regex = new RegExp("\{" + properties[i] + "\}", 'g');
-                            output = output.replace(regex, entry[properties[i]]);
-                        }
-                        searchResults.append($(output));
-                    }
-                });
-            }else{
-                searchResults.append( settings.noResults );
-            }
+		// Toggle on ESC key
+		window.addEventListener('keyup', function onKeyPress(e) {
+			if (e.which === 27) {
+				toggleSearch();
+			}
+		});
+		// Open on '/' key
+		window.addEventListener('keypress', function onKeyPress(e) {
+			if (e.which === 47 && !searchEl.classList.contains('is-active')) {
+				toggleSearch();
+			}
+		});
 
+		searchInputEl.addEventListener('input', function onInputChange() {
+			handleInput();
+		});
+	}
 
-        }
+	init.toggle = toggleSearch;
 
-        function clearSearchResults(){
-            searchResults.children().remove();
-        }
-    }
-}(Zepto));
+	window.superSearch = init;
+
+})();
