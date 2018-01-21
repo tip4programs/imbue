4e5f11d8c40f52e698aea4a4c78c5aacbf5bac71
chipprogrammers <arunksoman5678@gmail.com>
2018-01-21 20:35:28 +0530

    Update super-search.js

--- asset/js/super-search.js
+++ asset/js/super-search.js
@@ -1,34 +1,46 @@
-(function() {
+/*  Super Search
+    Author: Kushagra Gour (http://kushagragour.in)
+    MIT Licensed
+*/
+
+(function () {
     var isSearchOpen = false,
         searchEl = document.querySelector('#js-search'),
-        searchInputEl = document.querySelector('#js-search__input'),
-        searchResultsEl = document.querySelector('#js-search__results'),
+        searchInputEl = document.querySelector('#js-overlay__input'),
+        searchResultsEl = document.querySelector('#js-overlay__results'),
         currentInputValue = '',
-        lastSearchResultHash, posts = [],
+        lastSearchResultHash,
+        posts = [],
         sitemap = (baseurl || '') + '/sitemap.xml';
 
+    // Changes XML to JSON
+    // Modified version from here: http://davidwalsh.name/convert-xml-json
     function xmlToJson(xml) {
+
+        // Create the return object
         var obj = {};
-        if (xml.nodeType == 1) {
+
+        if (xml.nodeType == 1) { // element
+            // do attributes
             if (xml.attributes.length > 0) {
-                obj["@attributes"] = {};
+            obj["@attributes"] = {};
                 for (var j = 0; j < xml.attributes.length; j++) {
                     var attribute = xml.attributes.item(j);
                     obj["@attributes"][attribute.nodeName] = attribute.nodeValue;
                 }
             }
-        } else if (xml.nodeType == 3) {
+        } else if (xml.nodeType == 3) { // text
             obj = xml.nodeValue;
         }
-        var textNodes = [].slice.call(xml.childNodes).filter(function(node) {
-            return node.nodeType === 3;
-        });
+
+        // do children
+        // If all text nodes inside, get concatenated text from them.
+        var textNodes = [].slice.call(xml.childNodes).filter(function (node) { return node.nodeType === 3; });
         if (xml.hasChildNodes() && xml.childNodes.length === textNodes.length) {
-            obj = [].slice.call(xml.childNodes).reduce(function(text, node) {
-                return text + node.nodeValue;
-            }, '');
-        } else if (xml.hasChildNodes()) {
-            for (var i = 0; i < xml.childNodes.length; i++) {
+            obj = [].slice.call(xml.childNodes).reduce(function (text, node) { return text + node.nodeValue; }, '');
+        }
+        else if (xml.hasChildNodes()) {
+            for(var i = 0; i < xml.childNodes.length; i++) {
                 var item = xml.childNodes.item(i);
                 var nodeName = item.nodeName;
                 if (typeof(obj[nodeName]) == "undefined") {
@@ -50,30 +62,33 @@
         var json = xmlToJson(xml);
         return json.channel.item;
     }
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", sitemap);
-    xmlhttp.onreadystatechange = function() {
+
+    var xmlhttp=new XMLHttpRequest();
+    xmlhttp.open("GET",sitemap);
+    xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState != 4) return;
-        if (xmlhttp.status != 200 && xmlhttp.status != 304) {
-            return;
-        }
+        if (xmlhttp.status != 200 && xmlhttp.status != 304) { return; }
         var node = (new DOMParser).parseFromString(xmlhttp.responseText, 'text/xml');
         node = node.children[0];
         posts = getPostsFromXml(node);
     }
     xmlhttp.send();
+
     window.toggleSearch = function toggleSearch() {
         _gaq.push(['_trackEvent', 'supersearch', searchEl.classList.contains('is-active')]);
         searchEl.classList.toggle('is-active');
         if (searchEl.classList.contains('is-active')) {
+            // while opening
             searchInputEl.value = '';
         } else {
+            // while closing
             searchResultsEl.classList.add('is-hidden');
         }
-        setTimeout(function() {
+        setTimeout(function () {
             searchInputEl.focus();
         }, 210);
     }
+
     window.addEventListener('keyup', function onKeyPress(e) {
         if (e.which === 27) {
             toggleSearch();
@@ -84,8 +99,10 @@
             toggleSearch();
         }
     });
+
     searchInputEl.addEventListener('input', function onInputChange() {
         var currentResultHash, d;
+
         currentInputValue = (searchInputEl.value + '').toLowerCase();
         if (!currentInputValue || currentInputValue.length < 3) {
             lastSearchResultHash = '';
@@ -93,7 +110,8 @@
             return;
         }
         searchResultsEl.style.offsetWidth;
-        var matchingPosts = posts.filter(function(post) {
+
+        var matchingPosts = posts.filter(function (post) {
             if ((post.title + '').toLowerCase().indexOf(currentInputValue) !== -1 || (post.description + '').toLowerCase().indexOf(currentInputValue) !== -1) {
                 return true;
             }
@@ -101,16 +119,15 @@
         if (!matchingPosts.length) {
             searchResultsEl.classList.add('is-hidden');
         }
-        currentResultHash = matchingPosts.reduce(function(hash, post) {
-            return post.title + hash;
-        }, '');
+        currentResultHash = matchingPosts.reduce(function(hash, post) { return post.title + hash; }, '');
         if (matchingPosts.length && currentResultHash !== lastSearchResultHash) {
             searchResultsEl.classList.remove('is-hidden');
-            searchResultsEl.innerHTML = matchingPosts.map(function(post) {
+            searchResultsEl.innerHTML = matchingPosts.map(function (post) {
                 d = new Date(post.pubDate);
-                return '<li><a href="' + post.link + '">' + post.title + '<span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/, '$2 $1, $3') + '</span></a></li>';
+                return '<li><a href="' + post.link + '">' + post.title + '<span class="search__result-date">' + d.toUTCString().replace(/.*(\d{2})\s+(\w{3})\s+(\d{4}).*/,'$2 $1, $3') + '</span></a></li>';
             }).join('');
         }
         lastSearchResultHash = currentResultHash;
     });
+
 })();
