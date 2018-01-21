4a8cf2d8a3a4af9069994624c286e14850e7cc3d
chipprogrammers <arunksoman5678@gmail.com>
2018-01-21 19:38:39 +0530

    Update overlay.js

--- asset/js/overlay.js
+++ asset/js/overlay.js
@@ -1,20 +1,6 @@
-$(function () {
-    $('a[href="#search"]').on('click', function(event) {
-        event.preventDefault();
-        $('#search').addClass('open');
-        $('#search > form > input[type="search"]').focus();
-    });
-    
-    $('#search, #search button.close').on('click keyup', function(event) {
-        if (event.target == this || event.target.className == 'close' || event.keyCode == 27) {
-            $(this).removeClass('open');
-        }
-    });
-    
-    
-    //Do not include! This prevents the form from submitting for DEMO purposes only!
-    $('form').submit(function(event) {
-        event.preventDefault();
-        return false;
-    })
-});
+function openSearch() {
+    document.getElementById("myOverlay").style.display = "block";
+}
+function closeSearch() {
+    document.getElementById("myOverlay").style.display = "none";
+}
